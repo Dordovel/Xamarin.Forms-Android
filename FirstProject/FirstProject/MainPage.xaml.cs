bacdf9664e6f245ae653f8a9ffce2c0debb575ba
Dordovel <sernsft58@mail,ru>
2018-06-24 20:34:27 +0300

    Created Project

---
+++ FirstProject/FirstProject/MainPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using FirstProject.Model;
+using Xamarin.Forms;
+using File = Java.IO.File;
+
+namespace FirstProject
+{
+    public partial class MainPage : ContentPage
+    {
+        private File file;
+        private static string path;
+        private List<Files> list;
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+            path += "/";
+            FilePrint();
+
+        }
+
+        public MainPage(File file)
+        {
+            InitializeComponent();
+
+            this.file = file;
+
+            FilePrint();
+        }
+
+
+        private void FilePrint()
+        {
+            list = new List<Files>();
+
+            if (this.file == null)
+            {
+                this.file = new File("/");
+            }
+
+            path += file.Name;
+            Count.Text = path;
+            foreach (var VARIABLE in file.ListFiles())
+            {
+                if (VARIABLE.IsDirectory)
+                {
+                    list.Add(new Files() {FileName = VARIABLE.Name, Getfile = VARIABLE,Image = "folder.png"});
+                }
+                else if (VARIABLE.IsFile && Path.GetExtension(VARIABLE.Name).Equals("mp3"))
+                {
+                    list.Add(new Files() { FileName = VARIABLE.Name, Getfile = VARIABLE, Image = "music.png" });
+                }
+            }
+
+            listView.ItemsSource = list;
+        }
+
+        private void Open()
+        {
+            Count.Text = path;
+
+            path += ">";
+
+            Navigation.PushAsync(new MainPage(this.file));
+        }
+
+        private async void ListView_OnItemSelected(object sender, ItemTappedEventArgs e)
+        {
+            file = ((Files)listView.SelectedItem).Getfile;
+
+            switch (await DisplayActionSheet(file.Name, "Cancel", null, "Open"))
+            {
+                case "Open": Open();break;
+            }
+        }
+    }
+}
---
+++ FirstProject/FirstProject/Model/Files.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Android.Content.Res;
+using Java.IO;
+
+namespace FirstProject.Model
+{
+    public class Files
+    {
+        public string FileName { get; set; }
+        public File Getfile { get; set; }
+        public string Image { get; set; }
+    }
+}
